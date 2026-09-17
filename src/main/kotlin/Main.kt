d99b67a8379b466e9fd6023f040f5356ae9dc517
Gabriela Merino <gabrielamerino1103@gmail.com>
2026-09-16 21:11:46 -0600

    configurar proyecto Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("====================================")
+    println("         SUPER SELECTOS")
+    println("====================================")
+    println("Sistema de gestión de compras")
+    println("Etapa 2 - Desarrollo base")
+}
